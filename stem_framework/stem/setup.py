b4c221980b361f3103d337a4a833c12493eea0e2
Robert <fitagdinov.rr@phystech.edu>
2022-09-14 20:42:10 +0300

    point 5

--- stem_framework/stem/setup.py
+++ stem_framework/stem/setup.py
@@ -1,1 +1,11 @@
+from setuptools import setup, find_packages
 
+setup(
+   name='stem',
+   version='1.0',
+   description='A useful module',
+   author='Robert',
+   author_email='robertfitagdinov2gmail.com',
+   packages=['stem'],  #same as name
+   install_requires=['numpy', 'bar', 'greek'], #external packages as dependencies
+)
